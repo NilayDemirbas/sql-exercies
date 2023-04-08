39fb1078467f306e1b8eec62e1396d32b7a535ea
NilayDemirbas <114511116+NilayDemirbas@users.noreply.github.com>
2023-04-08 23:13:12 +0300

    Add files via upload

---
+++ sql-8.sql
@@ -0,0 +1,88 @@
+Merhabalar,
+
+1-test veritabanınızda employe isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+CREATE TABLE employe (
+    id INTEGER,
+    name VARCHAR(50),
+    birthday DATE ,
+    email VARCHAR(100)
+);
+
+
+
+2-Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+
+
+
+insert into employe (first_name, last_name, email, birthday) values ('Rene', 'Skala', 'rskala0@trellian.com', '2023-01-21');
+insert into employe (first_name, last_name, email, birthday) values ('Drugi', 'Bibey', 'dbibey1@squarespace.com', '2022-11-06');
+insert into employe (first_name, last_name, email, birthday) values ('Gabriela', 'Gounet', 'ggounet2@woothemes.com', '2022-10-05');
+insert into employe (first_name, last_name, email, birthday) values ('Dominik', 'Deble', 'ddeble3@google.fr', '2022-10-25');
+insert into employe (first_name, last_name, email, birthday) values ('Mandy', 'Freschini', 'mfreschini4@elegantthemes.com', '2023-03-20');
+insert into employe (first_name, last_name, email, birthday) values ('Brittaney', 'Legendre', 'blegendre5@china.com.cn', '2022-06-08');
+insert into employe (first_name, last_name, email, birthday) values ('Waldemar', 'Dowthwaite', 'wdowthwaite6@ovh.net', '2023-04-15');
+insert into employe (first_name, last_name, email, birthday) values ('Nomi', 'Waite', 'nwaite7@wikia.com', '2022-10-15');
+insert into employe (first_name, last_name, email, birthday) values ('Bobbie', 'Dunseath', 'bdunseath8@plala.or.jp', '2022-04-08');
+insert into employe (first_name, last_name, email, birthday) values ('Gretel', 'Lomis', 'glomis9@dot.gov', '2023-06-08');
+insert into employe (first_name, last_name, email, birthday) values ('Pyotr', 'Farnham', 'pfarnhama@joomla.org', '2023-11-26');
+insert into employe (first_name, last_name, email, birthday) values ('Charil', 'Campey', 'ccampeyb@tumblr.com', '2023-02-15');
+insert into employe (first_name, last_name, email, birthday) values ('Tracy', 'Castagneri', 'tcastagneric@hud.gov', '2023-08-01');
+insert into employe (first_name, last_name, email, birthday) values ('Kaiser', 'Woolfall', 'kwoolfalld@ucoz.ru', null);
+insert into employe (first_name, last_name, email, birthday) values ('Kristi', 'Romaynes', 'kromaynese@digg.com', '2022-04-25');
+insert into employe (first_name, last_name, email, birthday) values ('Blondelle', 'Duckers', 'bduckersf@wp.com', '2022-04-22');
+insert into employe (first_name, last_name, email, birthday) values ('Luca', 'Oliveras', 'loliverasg@rakuten.co.jp', '2023-08-22');
+insert into employe (first_name, last_name, email, birthday) values ('Farrell', 'Glaysher', 'fglaysherh@skype.com', '2023-08-31');
+insert into employe (first_name, last_name, email, birthday) values ('Paolo', 'Groundwater', 'pgroundwateri@yahoo.co.jp', '2022-08-10');
+insert into employe (first_name, last_name, email, birthday) values ('Ignazio', 'Rosborough', 'irosboroughj@wired.com', '2022-12-04');
+insert into employe (first_name, last_name, email, birthday) values ('Idaline', 'Trood', 'itroodk@squarespace.com', '2022-06-26');
+insert into employe (first_name, last_name, email, birthday) values ('Rikki', 'Plaice', 'rplaicel@livejournal.com', '2022-07-09');
+insert into employe (first_name, last_name, email, birthday) values ('Joella', 'Stobbie', 'jstobbiem@list-manage.com', '2023-01-20');
+insert into employe (first_name, last_name, email, birthday) values ('Ellynn', 'Milesap', 'emilesapn@de.vu', '2023-04-03');
+insert into employe (first_name, last_name, email, birthday) values ('Oralla', 'Worge', 'oworgeo@craigslist.org', '2022-11-05');
+insert into employe (first_name, last_name, email, birthday) values ('Freeman', 'Rosettini', 'frosettinip@ovh.net', '2023-09-14');
+insert into employe (first_name, last_name, email, birthday) values ('Rosita', 'Brittian', 'rbrittianq@economist.com', '2023-10-20');
+insert into employe (first_name, last_name, email, birthday) values ('Marchelle', 'Velez', 'mvelezr@businesswire.com', '2022-08-13');
+insert into employe (first_name, last_name, email, birthday) values ('Jefferey', 'Craik', 'jcraiks@state.gov', '2022-06-14');
+insert into employe (first_name, last_name, email, birthday) values ('Deeanne', 'Hazeldine', 'dhazeldinet@nymag.com', '2023-06-26');
+insert into employe (first_name, last_name, email, birthday) values ('Gertrud', 'Petricek', 'gpetriceku@unc.edu', '2022-06-07');
+insert into employe (first_name, last_name, email, birthday) values ('Giusto', 'Kirsche', 'gkirschev@amazon.co.jp', '2023-09-29');
+insert into employe (first_name, last_name, email, birthday) values ('Lyndsie', 'Brownbill', 'lbrownbillw@wired.com', null);
+insert into employe (first_name, last_name, email, birthday) values ('Buiron', 'Guerrero', 'bguerrerox@amazon.co.uk', '2023-01-01');
+insert into employe (first_name, last_name, email, birthday) values ('Loraine', 'Coakley', 'lcoakleyy@cbc.ca', '2023-11-11');
+insert into employe (first_name, last_name, email, birthday) values ('Claus', 'Albasiny', 'calbasinyz@usda.gov', '2023-01-27');
+insert into employe (first_name, last_name, email, birthday) values ('Meriel', 'Scriver', 'mscriver10@samsung.com', null);
+insert into employe (first_name, last_name, email, birthday) values ('Gilligan', 'Gentile', 'ggentile11@smh.com.au', '2023-06-21');
+insert into employe (first_name, last_name, email, birthday) values ('Josy', 'Figgs', 'jfiggs12@unc.edu', '2022-07-21');
+insert into employe (first_name, last_name, email, birthday) values ('Harp', 'Tavener', 'htavener13@a8.net', '2023-10-21');
+insert into employe (first_name, last_name, email, birthday) values ('Elroy', 'Poyntz', 'epoyntz14@senate.gov', '2023-10-06');
+insert into employe (first_name, last_name, email, birthday) values ('Rhett', 'Goodboddy', 'rgoodboddy15@creativecommons.org', '2023-04-22');
+insert into employe (first_name, last_name, email, birthday) values ('Thea', 'Carlet', 'tcarlet16@unc.edu', '2023-05-27');
+insert into employe (first_name, last_name, email, birthday) values ('Abbott', 'Bonnet', 'abonnet17@ft.com', '2022-12-09');
+insert into employe (first_name, last_name, email, birthday) values ('Alina', 'Tumielli', 'atumielli18@squarespace.com', '2022-11-06');
+insert into employe (first_name, last_name, email, birthday) values ('Allie', 'Leisk', 'aleisk19@ucoz.com', '2023-01-17');
+insert into employe (first_name, last_name, email, birthday) values ('Lesli', 'Kitchiner', 'lkitchiner1a@addthis.com', '2023-06-01');
+insert into employe (first_name, last_name, email, birthday) values ('Vaughn', 'Wimp', 'vwimp1b@xing.com', '2023-06-19');
+insert into employe (first_name, last_name, email, birthday) values ('Brier', 'Casino', 'bcasino1c@seattletimes.com', '2022-10-07');
+insert into employe (first_name, last_name, email, birthday) values ('Mord', 'Aguirrezabal', 'maguirrezabal1d@cbc.ca', '2022-04-11');
+
+
+3-Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+
+UPDATE employe
+    SET  first_name = "nilay "
+         last_name ="demirbaş" 
+         email = "nilay@gmail.com"
+         birthday "2000-11-27"
+WHERE first_name like "%n" ;
+
+
+4-Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+DELETE employe 
+WHERE  last_name ILIKE "(%n)";
+
+
+
+
+
+Kolay Gelsin.
